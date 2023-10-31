9977f7985fcdeeec3e7c5e04bd459bbb68dbff44
zoheb598 <joheb133@gmail.com>
2023-10-31 17:31:14 +0000

    Added "Home" popup page
    This page will be where the user can do various configurations while providing other features

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Add a click event listener to the button
+    const homeBtn = document.querySelector('.home-btn');
+    homeBtn.addEventListener('click', function () {
+        // Define the URL of your extension site page
+        const sitePageUrl = chrome.runtime.getURL('home.html');
+
+        // Use the chrome.tabs API to open a new tab with the site page URL
+        chrome.tabs.create({ url: sitePageUrl });
+    });
+});
