2c752bdf8b52341736cd409edecce1788e98462c
zoheb598 <joheb133@gmail.com>
2023-12-14 16:44:49 +0000

    Popup: Move sync & clear to options page

--- src/popup.js
+++ src/popup.js
@@ -1,30 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // Add a click event listener to the button
-    const homeBtn = document.querySelector('.home-btn');
-    homeBtn.addEventListener('click', function () {
+    const optionsBtn = document.querySelector('.options-btn');
+    optionsBtn.addEventListener('click', function () {
         // Define the URL of your extension site page
-        const sitePageUrl = chrome.runtime.getURL('src/pages/home/index.html');
+        const sitePageUrl = chrome.runtime.getURL('src/pages/options/index.html');
 
         // Use the chrome.tabs API to open a new tab with the site page URL
         chrome.tabs.create({ url: sitePageUrl });
     });
-
-    // Fetch data button
-    const fetchBtn = document.querySelector('.fetch-btn');
-    fetchBtn.addEventListener('click', function () {
-        chrome.runtime.sendMessage({ type: 'getDuolingo' }).then((res) => {
-            if (res.error) {
-                console.error('Error syncing Duolingo', res.error)
-            } else {
-                console.log('Data:', res.data);
-            }
-        })
-    });
-
-    // Clear stored data
-    const clearBtn = document.querySelector('.clear-btn');
-    clearBtn.addEventListener('click', function () {
-        chrome.storage.local.clear(() => console.log("Local storage cleared"))
-    })
 });
