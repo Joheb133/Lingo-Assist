5ebf3cb2a37ea5cd1da2108432e53486bb4a8f01
zoheb598 <joheb133@gmail.com>
2024-01-18 23:52:01 +0000

    Options: [feat] group words together
    Show different tables for different parts of speech. Group words with common pos together

--- frontend/src/pages/options/scripts/gui/displayLocalVocab.js
+++ frontend/src/pages/options/scripts/gui/displayLocalVocab.js
@@ -9,11 +9,13 @@ import convertSnakeCase from '../../../../utils/convertSnakeCase.js'
 export default function displayLocalVocab(obj) {
     clearLocalVocab()
 
-    const tableEl = document.querySelector("#vocab-table")
-    const bodyEl = document.createElement('tbody')
-
     // Populate table body with vocab
     for (const [key, value] of Object.entries(obj)) {
+        if (!document.querySelector(`#${value.pos.toLowerCase()}-table`)) {
+            createTable(value.pos.toLowerCase())
+        }
+
+        const table = document.querySelector(`#${value.pos.toLowerCase()}-table`)
         const row = document.createElement("tr")
 
         const word = document.createElement("td")
@@ -24,16 +26,32 @@ export default function displayLocalVocab(obj) {
 
         row.append(word, translation)
 
-        bodyEl.appendChild(row)
+        table.append(row)
     }
-
-    tableEl.appendChild(bodyEl)
 }
 
 export function clearLocalVocab() {
     // Wipe table data
-    const table = document.querySelector("#vocab-table")
-    const headings = table.firstElementChild
-    table.innerHTML = ''
-    table.appendChild(headings)
+    const table = document.querySelector(".table-container")
+    table.innerHTML = '';
+}
+
+function createTable(pos) {
+    const tableContainer = document.querySelector('.table-container')
+    const tableWrap = document.createElement('div')
+    const tableEl = document.createElement('table')
+    tableEl.id = `${pos.toLowerCase()}-table`
+
+    const tHeading1 = document.createElement('th')
+    const tHeading2 = document.createElement('th')
+    tHeading1.innerText = 'Word'
+    tHeading2.innerText = 'Translation'
+    const tHeadingRow = document.createElement('tr')
+    const heading = document.createElement('h4')
+    heading.innerText = pos.charAt(0).toUpperCase() + pos.slice(1);
+    tHeadingRow.append(tHeading1, tHeading2)
+
+    tableEl.append(tHeadingRow)
+    tableWrap.append(heading, tableEl)
+    tableContainer.append(tableWrap)
 }
