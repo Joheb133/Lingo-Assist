e33241a3cd8140499327828ba42a600b9b025158
zoheb598 <joheb133@gmail.com>
2024-02-07 16:28:32 +0000

    Game: [feat] basic logic
    The word in the learning language shows up, you input the translation, if it's right the word you don't have to type it again, if it's wrong, you'll be tested on it again. There's also a restart button, to reset the game.
    
    Forget the styling, but there are things that still need to be added.

--- frontend/src/pages/game/scripts/main.js
+++ frontend/src/pages/game/scripts/main.js
@@ -1,3 +1,67 @@
 import { getLocalVocab } from "../../../messages.js";
 
-console.log("hello world")
+let wordIndex = 0;
+
+async function init() {
+    const combinedISO = localStorage.getItem('combinedISO')
+    const vocab = await getLocalVocab(combinedISO)
+
+    if (!vocab) {
+        console.error(`Error loading vocab on ${combinedISO}`)
+        return
+    }
+
+    const vocabArr = Object.entries(vocab)
+
+    const wordWrapEl = document.querySelector('.word-wrap')
+    wordWrapEl.innerHTML = `<span>${vocabArr[0][0]}</span>`
+    console.log(vocabArr[0][1][0].translation)
+
+    const inputEl = document.querySelector('.game-wrap input')
+    inputEl.addEventListener('keydown', inputFunc)
+
+    const restartBtn = document.querySelector('.restart-btn')
+    restartBtn.addEventListener('click', restartFunc)
+
+    function inputFunc(e) {
+        if (e.code !== 'Enter') return
+
+        const wordDataArr = vocabArr[0][1]
+        const firstWordData = wordDataArr[0]
+
+        // Compare word on screen with translation(s)
+
+        if (inputEl.value.toLowerCase() === firstWordData.translation.toLowerCase()) { // User input right
+            // Remove that word data from vocabArr
+            if (vocabArr[0][1].length >= 2) {
+                vocabArr[0][1].shift()
+            } else {
+                vocabArr.shift()
+            }
+            console.log(true, inputEl.value.toLowerCase(), firstWordData.translation.toLowerCase(), vocabArr)
+        } else {
+            // Move first word to end of array
+            vocabArr.push(vocabArr.shift());
+            console.log(false, inputEl.value.toLowerCase(), firstWordData.translation.toLowerCase(), vocabArr)
+        }
+
+        if (vocabArr.length === 0) {
+            wordWrapEl.innerHTML = `<span>Nice job!</span>`
+            inputEl.removeEventListener('keydown', inputFunc)
+        } else {
+            inputEl.value = '';
+            wordWrapEl.innerHTML = `<span>${vocabArr[0][0]}</span>`
+            console.log(vocabArr[0][1][0].translation)
+        }
+    }
+
+    function restartFunc() {
+        // Remove listeners
+        console.log('...Restarting game')
+        inputEl.removeEventListener('keydown', inputFunc)
+        restartBtn.removeEventListener('click', restartFunc)
+        init()
+    }
+}
+
+init()
