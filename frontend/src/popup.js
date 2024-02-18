7ec6e7f66adffbcd8814cc5a40fac843415d393c
zoheb598 <joheb133@gmail.com>
2024-02-18 22:32:26 +0000

    Popup: [feat] improve GUI

--- frontend/src/popup.js
+++ frontend/src/popup.js
@@ -22,54 +22,77 @@ document.addEventListener("DOMContentLoaded", async function () {
         chrome.tabs.create({ url: sitePageUrl });
     });
 
-    // Toggle slider button
+    // Toggle translations button
+    const translationsToggleWrap = document.querySelector('.translations-toggle-wrap')
+    const translationToggleSpan = document.querySelector('#translation-toggle-span')
     const toggleSliderBtn = document.querySelector('.switch input');
-    const applyContentScript = await getData('applyContentScript')
+    let applyContentScript = await getData('applyContentScript')
     if (applyContentScript === null) {
         console.log('applyContentScript is', null)
         chrome.storage.local.set(({ applyContentScript: 'true' }))
         applyContentScript = 'true';
     }
-    applyContentScript === 'true' ? toggleSliderBtn.checked = true : toggleSliderBtn.checked = false;
+
+    if (applyContentScript === 'true') {
+        toggleSliderBtn.checked = true;
+        translationToggleSpan.innerText = 'enabled'
+    } else if (applyContentScript === 'false') {
+        toggleSliderBtn.checked = false;
+        translationToggleSpan.innerText = 'disabled'
+        translationsToggleWrap.classList.toggle('bg-green-200')
+        translationsToggleWrap.classList.toggle('bg-red-200')
+    }
 
     toggleSliderBtn.addEventListener('change', async function () {
         const applyContentScript = await getData('applyContentScript')
         if (toggleSliderBtn.checked && applyContentScript === 'false') {
             console.log('Set to on')
             chrome.storage.local.set(({ applyContentScript: 'true' }))
+            translationToggleSpan.innerText = 'enabled'
         } else if (!toggleSliderBtn.checked && applyContentScript === 'true') {
             console.log('Set to off')
             chrome.storage.local.set(({ applyContentScript: 'false' }))
+            translationToggleSpan.innerText = 'disabled'
         }
+        translationsToggleWrap.classList.toggle('bg-green-200')
+        translationsToggleWrap.classList.toggle('bg-red-200')
     })
 
-    // Current domain
+    // Ignore/unignore the current domain
     const currentDomainContainer = document.querySelector('.current-domain-container')
     const currentDomainSpan = currentDomainContainer.querySelector('span')
     const ignoreCurrentDomainBtn = currentDomainContainer.querySelector('button')
 
     const tempIgnoredDomains = await getData('ignoredDomains')
-    const ignoredDomainArr = tempIgnoredDomains === null ? [] : tempIgnoredDomains;
+    let ignoredDomainArr;
+    if (tempIgnoredDomains === null) {
+        ignoredDomainArr = []
+        chrome.storage.local.set(({ ignoredDomains: [] }))
+    } else {
+        ignoredDomainArr = tempIgnoredDomains;
+    }
+
     const currentDomain = await getCurrentTabDomain();
     currentDomainSpan.innerText = currentDomain;
 
     if (ignoredDomainArr.includes(currentDomain)) {
         ignoreCurrentDomainBtn.innerText = 'Unignore domain'
-    } else {
-        ignoreCurrentDomainBtn.innerText = 'Ignore domain'
+        ignoreCurrentDomainBtn.classList.toggle('domain-btn')
     }
 
     ignoreCurrentDomainBtn.addEventListener('click', async () => {
-        if (!ignoredDomainArr.includes(currentDomain)) {
-            const ignoredDomainArr = await getData('ignoredDomains')
-            ignoredDomainArr.push(currentDomain)
-            chrome.storage.local.set(({ ignoredDomains: ignoredDomainArr }))
-            ignoreCurrentDomainBtn.innerText = 'Unignore domain'
-        } else {
-            const ignoredDomainArr = await getData('ignoredDomains')
-            const newArr = ignoredDomainArr.filter((element) => element !== currentDomain)
+        ignoreCurrentDomainBtn.disabled = true; // temp disable cuz async operations
+        const ignoredDomains = await getData('ignoredDomains')
+        if (ignoredDomains.includes(currentDomain)) { // remove current domain from ignore list
+            const newArr = ignoredDomains.filter((element) => element !== currentDomain)
             chrome.storage.local.set(({ ignoredDomains: newArr }))
             ignoreCurrentDomainBtn.innerText = 'Ignore domain'
+        } else { // add current domain to ignore list
+            ignoredDomains.push(currentDomain)
+            chrome.storage.local.set(({ ignoredDomains }))
+            ignoreCurrentDomainBtn.innerText = 'Unignore domain'
         }
+        ignoreCurrentDomainBtn.classList.toggle('domain-btn')
+        ignoreCurrentDomainBtn.disabled = false;
     })
 });
