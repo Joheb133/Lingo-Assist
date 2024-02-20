d6894259497872d1b889abe3c58c48fd11680c46
zoheb598 <joheb133@gmail.com>
2024-02-20 16:19:32 +0000

    Options: [feat] update GUI when vocab cleared

--- frontend/src/pages/options/scripts/main.js
+++ frontend/src/pages/options/scripts/main.js
@@ -8,12 +8,20 @@ duolingoSyncBtn.addEventListener('click', function () {
     initDuolingoSync()
 });
 
-/* REMOVE from development */
-// Clear stored data
+
+// Clear words from local storage
 const clearBtn = document.querySelector('.clear-btn');
-clearBtn.addEventListener('click', function () {
-    chrome.storage.local.clear(() => console.log('Local storage cleared'))
+clearBtn.addEventListener('click', async function () {
+    const combinedISO = await getData('combinedISO')
+    if (combinedISO === false) {
+        return
+    }
+    chrome.storage.local.set({ [combinedISO]: {} })
     clearVocab()
+
+    // Display words learned
+    const wordsLearnedEl = document.querySelector('.duolingo-msg-el #words-learned')
+    wordsLearnedEl.innerText = '';
 })
 
 // Handle translation button
