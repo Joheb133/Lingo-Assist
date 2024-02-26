61512b6651aa8981c3e1e765980fe1081e0150a1
zoheb598 <joheb133@gmail.com>
2024-02-26 21:15:08 +0000

    CS: stop using observers
    Stop using observers incase they're resource intensive. Also limit transforms to less than 5 per paragraph

--- frontend/src/contentScript.js
+++ frontend/src/contentScript.js
@@ -1,25 +1,25 @@
 /**
  * @param {string} key - key we want to access
  * @return {Promise<Object|Error>} - 
-*/
+ */
 async function getData(key) {
     try {
         const res = await chrome.runtime.sendMessage({ type: 'getData', key });
         if (res === null) {
             console.error(`No data found on ${key}.`);
-            return null
+            return null;
         }
         return res;
     } catch (error) {
         console.error(`Error returning vocab on ${key}.`, error);
-        return false
+        return false;
     }
 }
 
 async function getCurrentTabDomain() {
     try {
-        const res = await chrome.runtime.sendMessage({ type: 'getCurrentTabDomain' })
-        return res
+        const res = await chrome.runtime.sendMessage({ type: 'getCurrentTabDomain' });
+        return res;
     } catch (error) {
         console.error(`Error getting current tab`, error);
     }
@@ -33,10 +33,10 @@ async function getCurrentTabDomain() {
  */
 function convertSnakeCase(string, capitalizeFirstChar = false) {
     // replace underscore with space
-    let formattedText = string.replace(/_/g, ' ')
+    let formattedText = string.replace(/_/g, ' ');
     formattedText = capitalizeFirstChar ? formattedText.charAt(0).toUpperCase() + formattedText.slice(1) : formattedText;
 
-    return formattedText
+    return formattedText;
 }
 
 // Function to reverse the structure
@@ -48,13 +48,13 @@ function reverseStructure(wordsObj) {
             // Ignore verbs and infinitives for now
             if (dataEl.pos !== 'Verb' && dataEl.pos !== 'Infinitive') {
                 // If the key doesn't exist, add it to the object above
-                for (const translationEl of dataEl.translation) {
-                    if (!reversedData.hasOwnProperty(translationEl)) {
-                        reversedData[`${translationEl.toLowerCase()}`] = [];
+                for (const translation of dataEl.translations) {
+                    if (!reversedData.hasOwnProperty(translation)) {
+                        reversedData[`${translation.toLowerCase()}`] = [];
                     }
 
-                    let newWord = convertSnakeCase(word)
-                    reversedData[`${translationEl.toLowerCase()}`].push(newWord)
+                    let newWord = convertSnakeCase(word);
+                    reversedData[`${translation.toLowerCase()}`].push(newWord);
                 }
             }
         }
@@ -65,41 +65,34 @@ function reverseStructure(wordsObj) {
 
 async function returnTranslationMap() {
     const combinedISO = await getData('combinedISO');
-    const localData = await getData(combinedISO)
+    const localData = await getData(combinedISO);
 
     if (!localData) {
-        console.error(`Error loading data on ISO = ${combinedISO}`)
+        console.error(`Error loading data on ISO = ${combinedISO}`);
         return false;
     }
 
-    return reverseStructure(localData)
+    return reverseStructure(localData);
 }
 
-// Callback function when the observed element enters or exits the viewport
-function intersectionCallback(entries, wordMap, excludedTags, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            // Element is currently in the viewport
-            replaceWordsInElement(entry.target, wordMap, excludedTags, observer);
-        }
-    });
-};
-
-function replaceWordsInElement(element, wordMap, excludedTags, observer) {
+function replaceWordsInElement(element, wordMap, excludedTags) {
     const wordCounter = {};
-    for (const childNode of element.childNodes) {
+    const childNodes = element.childNodes;
+
+    for (let i = 0; i < childNodes.length; i++) {
+        const childNode = childNodes[i];
+
         if (childNode.nodeType === Node.TEXT_NODE) {
             const originalText = childNode.textContent;
 
             // Replace specific words based on the wordMap
             const newText = originalText.replace(/\b(\w+)\b/g, (word) => {
-                const lowerWord = word.toLowerCase()
+                const lowerWord = word.toLowerCase();
                 const replacement = wordMap[lowerWord];
+
                 if (replacement) {
-                    // Decide whether to replace word or not
-                    const randomNum = Math.floor(Math.random() * 2); // 50/50
-                    if (randomNum === 0) { // Outcome = 0 || 1
-                        return word;
+                    if (wordCounter[lowerWord] > 5) { // Limit transforms to less than 5
+                        return word
                     }
 
                     // Div container for elements
@@ -111,7 +104,7 @@ function replaceWordsInElement(element, wordMap, excludedTags, observer) {
                     translationSpan.classList.add('lingo-assist-span');
 
                     // Use different translations
-                    wordCounter[lowerWord] !== undefined ? wordCounter[lowerWord]++ : wordCounter[lowerWord] = 0;
+                    wordCounter[lowerWord] !== undefined ? wordCounter[lowerWord]++ : wordCounter[lowerWord] = 1;
                     const index = wordCounter[lowerWord] % replacement.length;
                     translationSpan.textContent = replacement[index];
 
@@ -125,6 +118,7 @@ function replaceWordsInElement(element, wordMap, excludedTags, observer) {
 
                     return container.outerHTML;
                 }
+
                 return word;
             });
 
@@ -136,43 +130,52 @@ function replaceWordsInElement(element, wordMap, excludedTags, observer) {
             element.replaceChild(tempElement.content, childNode);
         } else if (childNode.nodeType === Node.ELEMENT_NODE && !excludedTags.includes(childNode.tagName.toLowerCase())) {
             // Recursively handle child elements
-            replaceWordsInElement(childNode, wordMap, excludedTags, observer);
+            replaceWordsInElement(childNode, wordMap, excludedTags);
+        }
+    }
+}
+
+async function transformPageInChunks(wordMap, excludedTags, chunkSize, timeout) {
+    const allParagraphs = document.querySelectorAll('p');
+    const paragraphsCount = allParagraphs.length;
+
+    let currentIndex = 0;
+
+    function processNextChunk() {
+        const endIndex = Math.min(currentIndex + chunkSize, paragraphsCount);
+
+        for (let i = currentIndex; i < endIndex; i++) {
+            replaceWordsInElement(allParagraphs[i], wordMap, excludedTags);
+        }
+
+        currentIndex = endIndex;
+
+        if (currentIndex < paragraphsCount) {
+            setTimeout(processNextChunk, timeout);
         }
     }
 
-    // Remove intersection observer
-    observer.unobserve(element)
-};
+    processNextChunk();
+}
 
 async function main() {
-    const currentDomain = await getCurrentTabDomain()
-    const tempIgnoredDomains = await getData('ignoredDomains')
+    const currentDomain = await getCurrentTabDomain();
+    const tempIgnoredDomains = await getData('ignoredDomains');
     const ignoredDomains = tempIgnoredDomains === null ? [] : tempIgnoredDomains;
-    const applyContentScript = await getData('applyContentScript')
+    const applyContentScript = await getData('applyContentScript');
+
     if (applyContentScript === 'false' || ignoredDomains.includes(currentDomain)) {
-        return
+        return;
     }
 
     console.log("Lingo Assist content script activated");
 
     const wordMap = await returnTranslationMap();
+    const excludedTags = ['a', 'span']; // Tags to not transform
+    const chunkSize = 20;
+    const timeout = 2000;
 
-    // Options for the Intersection Observer
-    const intersectionOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.5, // Trigger when at least 50% of the element is visible
-    };
-
-    // Exlcuded elements
-    const excludedTags = ['a', 'span']
-
-    // Create an Intersection Observer
-    const intersectionObserver = new IntersectionObserver((entries) => intersectionCallback(entries, wordMap, excludedTags, intersectionObserver), intersectionOptions);
-
-    // Find and observe target elements (e.g., all paragraphs)
-    const targetElements = document.querySelectorAll('p');
-    targetElements.forEach(element => intersectionObserver.observe(element));
+    transformPageInChunks(wordMap, excludedTags, chunkSize, timeout);
 }
 
-main()
+main();
